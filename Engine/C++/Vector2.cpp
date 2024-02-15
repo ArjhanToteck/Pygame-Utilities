7d67885ef0abcf8d48a1ce9fa73d6d82a5506fb8
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-02-14 21:28:18 -0700

    c++ library progress
    we got some basic constructors, getters, and setters for vector2 as a test. hopefully ill rewrite all the bad python code as faster (but still probably bad) c++ code

--- Engine/C++/Vector2.cpp
+++ Engine/C++/Vector2.cpp
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <iostream>
 
@@ -43,43 +44,88 @@ void Vector2_delete(PyObject* capsule) {
 }
 
 static PyObject* Vector2_new(PyObject* self, PyObject* args) {
-    double x = 0, y = 0;
+    // default x and y
+    double x = 0;
+    double y = 0;
 
-    if (!PyArg_ParseTuple(args, "|dd", &x, &y)) {
-        return NULL; // Error parsing arguments
-    }
+    // get x and y parameters from args
+    PyArg_ParseTuple(args, "dd", &x, &y);
 
+    // create Vector2
     Vector2* vector = new Vector2(x, y);
+
+    // make sure it was properly constructed
     if (!vector) {
-        PyErr_SetString(PyExc_RuntimeError, "Failed to create Vector2 object");
+        PyErr_SetString(PyExc_RuntimeError, "Failed to create a new Vector2 object.");
         return NULL;
     }
 
+    // return as pycapsule
     return PyCapsule_New(vector, "Vector2", Vector2_delete);
 }
 
 // getters
 
-static PyObject* Vector2_getX(PyObject* self) {
-    Vector2* vector = (Vector2*)PyCapsule_GetPointer(self, "Vector2");
+static PyObject* Vector2_getX(PyObject* self, PyObject* args) {
+    // get capsule parameter
+    PyObject* capsule;
 
-    return Py_BuildValue("d", vector->getX());
+    if (!PyArg_ParseTuple(args, "O", &capsule)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid capsule object passed.");
+        return NULL;
+    }
+
+    // convert capsule to Vector2
+    Vector2* vector = static_cast<Vector2*>(PyCapsule_GetPointer(capsule, "Vector2"));
+
+    if (!vector) {
+        PyErr_SetString(PyExc_TypeError, "The passed capsule object is not a Vector2.");
+        return NULL;
+    }
+
+    // return x value as python float
+    return PyFloat_FromDouble(vector->getX());
 }
 
 
-static PyObject* Vector2_getY(PyObject* self) {
-    Vector2* vector = (Vector2*)PyCapsule_GetPointer(self, "Vector2");
+static PyObject* Vector2_getY(PyObject* self, PyObject* args) {
+    // get capsule parameter
+    PyObject* capsule;
 
-    return Py_BuildValue("d", vector->getY());
+    if (!PyArg_ParseTuple(args, "O", &capsule)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid capsule object passed.");
+        return NULL;
+    }
+
+    // convert capsule to Vector2
+    Vector2* vector = static_cast<Vector2*>(PyCapsule_GetPointer(capsule, "Vector2"));
+
+    if (!vector) {
+        PyErr_SetString(PyExc_TypeError, "The passed capsule object is not a Vector2.");
+        return NULL;
+    }
+
+    // return y value as python float
+    return PyFloat_FromDouble(vector->getY());
 }
 
 // setters
 
-static PyObject* Vector2_setX(PyObject* self, PyObject* args) {
-    Vector2* vector = (Vector2*)PyCapsule_GetPointer(self, "Vector2");
+static PyObject* Vector2_setX(Vector2* self, PyObject* args) {
+    // get parameters
+    PyObject* capsule;
     double newX;
 
-    if (!PyArg_ParseTuple(args, "d", &newX)) {
+    if (!PyArg_ParseTuple(args, "Od", &capsule, &newX)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid parameters passed.");
+        return NULL;
+    }
+
+    // convert capsule from parameter to Vector2
+    Vector2* vector = static_cast<Vector2*>(PyCapsule_GetPointer(capsule, "Vector2"));
+
+    if (!vector) {
+        PyErr_SetString(PyExc_TypeError, "The passed capsule object is not a Vector2.");
         return NULL;
     }
 
@@ -88,24 +134,36 @@ static PyObject* Vector2_setX(PyObject* self, PyObject* args) {
 }
 
 
-static PyObject* Vector2_setY(PyObject* self, PyObject* args) {
-    Vector2* vector = (Vector2*)PyCapsule_GetPointer(self, "Vector2");
+static PyObject* Vector2_setY(Vector2* self, PyObject* args) {
+    // get parameters
+    PyObject* capsule;
     double newY;
 
-    if (!PyArg_ParseTuple(args, "d", &newY)) {
+    if (!PyArg_ParseTuple(args, "Od", &capsule, &newY)) {
+        PyErr_SetString(PyExc_TypeError, "Invalid parameters passed.");
         return NULL;
     }
 
+    // convert capsule from parameter to Vector2
+    Vector2* vector = static_cast<Vector2*>(PyCapsule_GetPointer(capsule, "Vector2"));
+
+    if (!vector) {
+        PyErr_SetString(PyExc_TypeError, "The passed capsule object is not a Vector2.");
+        return NULL;
+    }
+
+    // set new y
     vector->setY(newY);
+
     Py_RETURN_NONE;
 }
 
 // set python methods
 static PyMethodDef pythonMethods[] = {
-    {"Vector2_new", (PyCFunction)Vector2_getX, METH_VARARGS, "Create new Vector2"},
-    {"Vector2_delete", (PyCFunction)Vector2_getX, METH_NOARGS, "Delete a Vector2"},
-    {"Vector2_getX", (PyCFunction)Vector2_getX, METH_NOARGS, "Get X of Vector2 class"},
-    {"Vector2_getY", (PyCFunction)Vector2_getY, METH_NOARGS, "Get Y of Vector2 class"},
+    {"Vector2_new", (PyCFunction)Vector2_new, METH_VARARGS, "Create new Vector2"},
+    {"Vector2_delete", (PyCFunction)Vector2_getX, METH_VARARGS, "Delete a Vector2"},
+    {"Vector2_getX", (PyCFunction)Vector2_getX, METH_VARARGS, "Get X of Vector2 class"},
+    {"Vector2_getY", (PyCFunction)Vector2_getY, METH_VARARGS, "Get Y of Vector2 class"},
     {"Vector2_setX", (PyCFunction)Vector2_setX, METH_VARARGS, "Set X of Vector2 class"},
     {"Vector2_setY", (PyCFunction)Vector2_setY, METH_VARARGS, "Set Y of Vector2 class"},
     {NULL, NULL, 0, NULL}
--- Engine/C++/main.py
+++ Engine/C++/main.py
@@ -1,6 +1,10 @@
 import Vector2
 
-print(Vector2.Vector2_new)
-
-vector2 = Vector2.Vector2_new()
+vector2 = Vector2.Vector2_new(1, 2)
 print(Vector2.Vector2_getX(vector2))
+print(Vector2.Vector2_getY(vector2))
+
+Vector2.Vector2_setX(vector2, 312)
+Vector2.Vector2_setY(vector2, 67)
+print(Vector2.Vector2_getX(vector2))
+print(Vector2.Vector2_getY(vector2))
