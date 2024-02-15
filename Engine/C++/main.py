27f78b763aec1b0649b1447c505d3a1a2f59858f
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-02-15 11:59:25 -0700

    basic class wrapper

--- Engine/C++/main.py
+++ Engine/C++/main.py
@@ -1,10 +1,34 @@
-import Vector2
+import Vector2 as Vector2Cpp
 
-vector2 = Vector2.Vector2_new(1, 2)
-print(Vector2.Vector2_getX(vector2))
-print(Vector2.Vector2_getY(vector2))
+class Vector2:
+    def __init__(self, x=0, y=0):
+        self.__nativeVector2 = Vector2Cpp.Vector2_new(x, y)
 
-Vector2.Vector2_setX(vector2, 312)
-Vector2.Vector2_setY(vector2, 67)
-print(Vector2.Vector2_getX(vector2))
-print(Vector2.Vector2_getY(vector2))
+
+    # getter wrappers
+
+    @property
+    def x(self):
+        return Vector2Cpp.Vector2_getX(self.__nativeVector2)
+    
+
+    @property
+    def y(self):
+        return Vector2Cpp.Vector2_getY(self.__nativeVector2)
+
+    # setter wrappers
+
+    @x.setter
+    def x(self, value):
+        Vector2Cpp.Vector2_setX(self.__nativeVector2, value)
+
+
+    @y.setter
+    def y(self, value):
+        Vector2Cpp.Vector2_setY(self.__nativeVector2, value)
+
+
+vector = Vector2(3, 1)
+print(vector.x)
+vector.x = 123
+print(vector.x)
